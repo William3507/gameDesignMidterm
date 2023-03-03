4646b71080231bf1400bb9ab227392ff39477d72
Madi McDougall <mmcdouga9466@scrippscollege.edu>
2023-03-03 12:24:32 +0100

    Animations added, UI functional (hopefully)

--- miceGame/Assets/Scripts/Mouse/enemyController.cs
+++ miceGame/Assets/Scripts/Mouse/enemyController.cs
@@ -5,15 +5,23 @@ using UnityEngine;
 public class enemyController : MonoBehaviour
 {
     public LayerMask platforms;
-    public bool grounded;
-    public float lengthRay;
+    public float lengthRay = .9f;
     private bool invincible = false;
+    public int hurtTime = 1;
 
     public float health = 3;
 
+    public Sprite[] idle;
+    public Sprite[] WalkCycle;
+    public Sprite[] Hide;
+
+    private Sprite[] currentCycle;
+    public float fps = 8;
+    private int currentFrame = 0;
+    bool blinking = false;
 
     protected Rigidbody2D rb2d;
-    protected SpriteRenderer spriteRenderer;
+    protected SpriteRenderer sr;
     
 
 
@@ -21,29 +29,52 @@ public class enemyController : MonoBehaviour
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
-        spriteRenderer = GetComponent<SpriteRenderer>();
+        sr = GetComponent<SpriteRenderer>();
+
+        currentCycle = idle;
+        StartCoroutine(AnimationCycler());
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        
-            RaycastHit2D upHit = Physics2D.Raycast(transform.position, Vector2.up, lengthRay, LayerMask.GetMask("Player"));
-            RaycastHit2D leftUpHit = Physics2D.Raycast(transform.position, new Vector2(-0.5f, 1f), lengthRay, LayerMask.GetMask("Player"));
-            RaycastHit2D rightUpHit = Physics2D.Raycast(transform.position, new Vector2(0.5f, 1f), lengthRay, LayerMask.GetMask("Player"));
 
-            Debug.DrawLine(transform.position, Vector2.up * lengthRay * 2, Color.blue);
+        RaycastHit2D upHit = Physics2D.Raycast(transform.position, Vector2.up, lengthRay, LayerMask.GetMask("Player"));
+        RaycastHit2D leftUpHit = Physics2D.Raycast(transform.position, new Vector2(-0.5f, 1f), lengthRay, LayerMask.GetMask("Player"));
+        RaycastHit2D rightUpHit = Physics2D.Raycast(transform.position, new Vector2(0.5f, 1f), lengthRay, LayerMask.GetMask("Player"));
+
+        Debug.DrawLine(transform.position, Vector2.up * lengthRay * 2, Color.blue);
 
 
 
         if (upHit.collider != null || leftUpHit.collider != null || rightUpHit.collider != null)
-            {
-                Hurt();
-            }
-        
+        {
+            Hurt();
+        }
+
+        if (rb2d.velocity.x > 0)
+        {
+            transform.localScale = new Vector3(1f, 1f, 1f);
+        }
+        else if (rb2d.velocity.x < 0)
+        {
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+        }
+
+        if (rb2d.velocity.x != 0)
+        {
+            currentCycle = WalkCycle;
+
+        }
+        else if (health == 1)
+        {
+            currentCycle = Hide;
+        }
+        else
+        {
+            currentCycle = idle;
+        }
     }
-
-
 
     public void Hurt()
     {        
@@ -72,15 +103,43 @@ public class enemyController : MonoBehaviour
     {
         invincible = true;
 
-        for (int i = 0; i < time / 0.4f; i++)
-        {
-            spriteRenderer.color = Color.grey;
-            yield return new WaitForSeconds(0.2f);
-            spriteRenderer.color = Color.magenta;
-            yield return new WaitForSeconds(0.2f);
-        }
+        yield return new WaitForSeconds(hurtTime);
 
         invincible = false;
 
+    }
+    IEnumerator AnimationCycler()
+    {
+        while (true)
+        {
+            if (invincible)
+            {
+                blinking = !blinking;
+            }
+            else
+            {
+                blinking = false;
+            }
+
+            if (blinking)
+            {
+                sr.color = Color.red;
+            }
+            else
+            {
+                sr.color = Color.white;
+            }
+
+            if (currentFrame >= currentCycle.Length - 1)
+            {
+                currentFrame = 0;
+            }
+            else
+            {
+                currentFrame++;
+            }
+            sr.sprite = currentCycle[currentFrame];
+            yield return new WaitForSeconds(1f / fps);
+        }
     }
 }
