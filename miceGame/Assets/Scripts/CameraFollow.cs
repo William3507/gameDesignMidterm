fe14ca6f6d64a9fe62798de1374b2626e498e5c9
tristanmeyer2 <97000978+tristanmeyer2@users.noreply.github.com>
2023-03-03 12:58:39 +0100

    starting level without final touches

--- miceGame/Assets/Scripts/CameraFollow.cs
+++ miceGame/Assets/Scripts/CameraFollow.cs
@@ -4,18 +4,13 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    public Transform target;
-    public Vector3 offset = new Vector3(0, 2, -10);
-    public float smoothTime = 0.25f;
+    public Transform player;
+    public Vector3 camOffset = new Vector3(0, 2, -10);
+    public float delay = 0.25f;
     Vector3 currentVelocity;
 
     private void LateUpdate()
     {
-        transform.position = Vector3.SmoothDamp(
-            transform.position,
-            target.position + offset,
-            ref currentVelocity,
-            smoothTime
-            );
+        transform.position = Vector3.SmoothDamp(transform.position,player.position + camOffset,ref currentVelocity,delay);
     }
 }
